ffcdb89caf8d5f7017c592cafc8c7d8259563c8f
Aleksandr <sheph@ukr.net>
2021-11-24 22:05:01 +0200

    added header, partner section, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,23 @@
 $(function () {
 
 
- 
+    $('.header-top__mob-menu').on('click', function (event) {
+        $('.header-top__mob-menu').toggleClass('header-top__mob-menu-active');
+        $('.header-menu').toggleClass('header-menu__active');
+        $('body').toggleClass('lock');
+    });
+
+
+
+    $('.partrtner__btn').on('click', function (e) {
+
+        e.preventDefault();
+        $('.partrtner__btn').removeClass('partrtner__btn--active');
+        $(this).addClass('partrtner__btn--active');
+
+        $('.partrtner__block').removeClass('partrtner__block--active');
+        $($(this).attr('href')).addClass('partrtner__block--active');
+
+    });
 
 });
